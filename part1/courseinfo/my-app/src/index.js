d0c2d3b18fa71faedfaa7a6df8da62cde502066f
Huiyu <iownthegame@gmail.com>
2020-07-27 22:31:50 +0200

    ex 1.2 - refactor Content

--- part1/courseinfo/my-app/src/index.js
+++ part1/courseinfo/my-app/src/index.js
@@ -5,18 +5,18 @@ const Header = (props) => (
   <h1>{props.course}</h1>
 )
 
+const Part = (props) => (
+  <p>
+    {props.name} {props.number}
+  </p>
+)
+
 const Content = (props) => (
-  <>
-    <p>
-      {props.part1} {props.exercises1}
-    </p>
-    <p>
-      {props.part2} {props.exercises2}
-    </p>
-    <p>
-      {props.part3} {props.exercises3}
-    </p>
-  </>
+  <div>
+    <Part name={props.part1} number={props.exercises1} />
+    <Part name={props.part2} number={props.exercises2} />
+    <Part name={props.part3} number={props.exercises3} />
+  </div>
 )
 
 const Total = (props) => (
