15869d94d1c84555fa69c2f068ad21fcfde84662
Huiyu <iownthegame@gmail.com>
2020-08-11 14:43:33 +0200

    ex 2.11 - axios, useEffect

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,12 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+
 import Filter from './components/Filter'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-
+  const [ persons, setPersons ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filterName, setFilterName ] = useState('')
@@ -45,6 +41,20 @@ const App = () => {
   const handleFilterChange = (event) => {
     setFilterName(event.target.value)
   }
+
+  const hook = () => {
+    console.log('effect')
+
+    const eventHandler = response => {
+      console.log('promise fulfilled')
+      setPersons(response.data)
+    }
+
+    const promise = axios.get('http://localhost:3001/persons')
+    promise.then(eventHandler)
+  }
+
+  useEffect(hook, [])
 
   const showPersons = filterName ? persons.filter(person => person.name.toLowerCase().includes(filterName.toLowerCase())) : persons
 
