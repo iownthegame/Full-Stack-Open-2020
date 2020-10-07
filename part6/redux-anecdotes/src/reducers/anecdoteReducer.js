f785400995bbce7addf1c6a284e26b6246bc0804
Huiyu <iownthegame@gmail.com>
2020-10-07 23:43:52 +0200

    ex 6.15 - thunk, async initialize anecdotes

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,3 +1,5 @@
+import anecdoteService from '../services/anecdotes'
+
 export const addVote = (id) => {
   return {
     type: 'ADD_VOTE',
@@ -12,10 +14,13 @@ export const createAnecdote = (data) => {
   }
 }
 
-export const initializeAnecdotes = (anecdotes) => {
-  return {
-    type: 'INIT_ANECDOTES',
-    data: anecdotes
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch({
+      type: 'INIT_ANECDOTES',
+      data: anecdotes
+    })
   }
 }
 
