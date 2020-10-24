92aa358ed462435ff30fb12e1f33975798562912
Huiyu <iownthegame@gmail.com>
2020-10-24 12:26:35 +0200

    ex 9.1 - setup typescript, add bmiCalculator

---
+++ part9/ts_project/bmiCalculator.ts
@@ -0,0 +1,24 @@
+function calculateBmi(height: number, weight: number) {
+  const BMI = weight / ((height / 100) * (height / 100))
+  let res
+  if (BMI < 15)
+    res = "Very severely underweight"
+  else if (BMI < 16)
+    res = "Severely underweight"
+  else if (BMI < 18.5)
+    res = "Underweight"
+  else if (BMI < 25)
+    res = "Normal (healthy weight)"
+  else if (BMI < 30)
+    res = "Overweight"
+  else if (BMI < 35)
+    res = "Obese Class I (Moderately obese)"
+  else if (BMI < 40)
+    res = "Obese Class II (Severely obese)"
+  else
+    res = "Obese Class III (Very severely obese)"
+
+  return res
+}
+
+console.log(calculateBmi(180, 74))
